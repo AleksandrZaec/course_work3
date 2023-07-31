ff5c95492c6fd64d607066b74062044058b853ba
AleksandrZaec <zaecam95@mail.ru>
2023-07-31 11:07:27 +0300

    add utils

---
+++ utils/__init__.py
@@ -0,0 +1,22 @@
+def filter_operations():
+    return None
+
+
+def mask_card_number():
+    return None
+
+
+def print_operation():
+    return None
+
+
+def read_operations():
+    return None
+
+
+def sort_operations():
+    return None
+
+
+def get_last_operations():
+    return None
